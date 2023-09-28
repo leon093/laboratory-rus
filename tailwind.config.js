3cc5ccac52abbcbc305c09a3635335a49b49a199
kfqr <ilja.golubev@gmail.com>
2023-09-28 22:49:49 +0300

    second mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["./src/pages/*.html", "./src/index.html"],
   theme: {
+    listStyleType: {
+      none: "none",
+      decimal: "decimal",
+      square: "square",
+    },
     extend: {
       fontFamily: {
         sans: ["ristretto", ...defaultTheme.fontFamily.sans],
