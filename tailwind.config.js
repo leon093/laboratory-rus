d3ffa31b160d35068df1453279c36ee63beb42f5
KFQR <ilja.golubev@gmail.com>
2023-10-10 20:58:16 +0300

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
-  content: ["./src/pages/*.html", "./src/index.html", "./src/public/assets/map.js"],
+  content: [
+    "./src/pages/*.html",
+    "./src/index.html",
+    "./src/public/assets/map.js",
+  ],
   theme: {
     listStyleType: {
       none: "none",
       decimal: "decimal",
       square: "square",
+    },
+    screens: {
+      "3xl": "1920px",
+      ...defaultTheme.screens,
     },
     extend: {
       fontFamily: {
@@ -15,6 +23,6 @@ export default {
       },
     },
   },
-  safelist: ['ya-gray-pane'],
+  safelist: ["ya-gray-pane"],
   plugins: [],
 };
