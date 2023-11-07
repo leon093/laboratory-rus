b277f96f0b8533dd31c1645d3af0d1cab5f3daaf
vostretsov <vostretsov@ooo-alfatel.ru>
2023-11-07 11:52:17 +0300

    Вынес красный цвет в тему tailwincss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
-  content: [
-    "./src/pages/*.html",
-    "./src/index.html",
-    "./src/public/assets/map.js",
-  ],
-  theme: {
-    listStyleType: {
-      none: "none",
-      decimal: "decimal",
-      square: "square",
+    content: ["./src/pages/*.html", "./src/index.html", "./src/public/assets/map.js"],
+    theme: {
+        listStyleType: {
+            none: "none",
+            decimal: "decimal",
+            square: "square",
+        },
+        screens: {
+            "3xl": "1920px",
+            ...defaultTheme.screens,
+        },
+        extend: {
+            fontFamily: {
+                sans: ["ristretto", ...defaultTheme.fontFamily.sans],
+                copy: ["Arial, Helvetica, sans-serif"],
+            },
+            colors: {
+                primary: {
+                    50: "#fff0f0",
+                    100: "#ffdede",
+                    200: "#ffc3c3",
+                    300: "#ff9999",
+                    400: "#ff5f5e",
+                    500: "#ff2d2c",
+                    600: "#f62221",
+                    700: "#cf0706",
+                    800: "#aa0b0a",
+                    900: "#8d100f",
+                    950: "#4d0202",
+                },
+            },
+        },
     },
-    screens: {
-      "3xl": "1920px",
-      ...defaultTheme.screens,
-    },
-    extend: {
-      fontFamily: {
-        sans: ["ristretto", ...defaultTheme.fontFamily.sans],
-        copy: ["Arial, Helvetica, sans-serif"],
-      },
-    },
-  },
-  safelist: ["ya-gray-pane"],
-  plugins: [],
+    safelist: ["ya-gray-pane"],
+    plugins: [],
 };
