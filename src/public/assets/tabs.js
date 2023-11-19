58346461057ce3951e040247a6ed6e1d4e6077a1
leon <leon093@gmail.com>
2023-11-19 21:24:12 +0300

    тестовая вёрстка табов

--- src/public/assets/tabs.js
+++ src/public/assets/tabs.js
@@ -1,51 +1,63 @@
 function tabs(props = {}) {
-    if (typeof props !== 'object') {
-        throw new Error('props must be an object');
+    if (typeof props !== "object") {
+        throw new Error("props must be an object");
     }
 
     const defaultProps = {
-        btn: '.js-tab-btn',
-        content: '.js-tab-content',
-        btnText: '.js-text'
-    }
-
-    let activeTabIndex = null;
-    let originalTitles = [];
+        btn: ".js-tab-btn",
+        content: ".js-tab-content",
+        btnText: ".js-text",
+    };
 
     const { btn, content, btnText } = { ...defaultProps, ...props };
-    const [buttonsElem, contentsElem] = [btn, content].map(selector => document.querySelectorAll(selector));
+    const [buttonsElem, contentsElem, btnsTextElem] = [btn, content, btnText].map((selector) => document.querySelectorAll(selector));
+
+    let activeTabIndex = null;
+    let titleWords = Array.from(btnsTextElem, elem => elem.textContent);
+    let titles = titleWords.reduce((acc, val, i) => {
+        i % 2 === 0 ? acc.push([val]) : acc[acc.length - 1].push(val)
+        return acc;
+    }, []);
 
     const activeTab = (btnElement, contentElement, i) => {
+        buttonsElem.forEach((btn) => btn.classList.remove("active"));
+        contentsElem.forEach((content) => content.classList.add("hidden"));
+        btnsTextElem.forEach((elem, i) => elem.textContent = titleWords[i]);
 
-        // Check if the same tab is clicked again
-        if (i === activeTabIndex) {
-            // Restore the original title
-            btn.innerHTML = originalTitles[i];
+        btnElement.classList.add("active");
+        contentElement.classList.remove("hidden");
+        let titleTextElems = btnElement.querySelectorAll(btnText);
+        let titleTextServices = ['Наши', 'услуги'];
+
+        if (titleTextElems.length >= 2) {
+            titleTextElems.forEach((elem, i) => elem.textContent = titleTextServices[i])
         } else {
-            // Change the title
-            btn.innerHTML = 'New Title';
+            titleTextElems[0].textContent = titleTextServices.join(' ');
         }
 
-        buttonsElem.forEach(btn => btn.classList.remove('active'));
-        contentsElem.forEach(content => content.classList.add('hidden'));
+        if (i === activeTabIndex) {
+            clickCount++
 
-        btnElement.classList.add('active');
-        contentElement.classList.remove('hidden');
-
-        let titles = btnElement.querySelectorAll(btnText);
-        let copyTitle = [...titles];
-        copyTitle[0].textContent = 'Наши';
-        copyTitle[1].textContent = 'услуги';
-    }
+            if (clickCount === 2) {
+                contentElement.classList.remove("hidden");
+                clickCount = 0
+            } else {
+                titleTextElems.forEach((elem, elemIndex) => elem.textContent = titles[i][elemIndex])
+                contentsElem.forEach((content) => content.classList.add("hidden"));
+                contentsElem[0].classList.remove('hidden');
+            }
+        } else {
+            clickCount = 0
+        }
+    };
 
     buttonsElem.forEach((btn, i) => {
-        btn.addEventListener('click', (e) => {
+        btn.addEventListener("click", (e) => {
             e.preventDefault();
-
             activeTab(btn, contentsElem[i + 1], i);
             activeTabIndex = i;
-        })
-    })
-}
+        });
+    });
+ }
 
-tabs();
+ tabs();
